2610f7e78655efcd7ccead75451cdabf2f5c6b92
Michael Misch <michealmisch1985@gmail.com>
2021-09-26 12:43:13 -0700

    Update so that builds work in a modern go toolchain
     - Add in temporary errors.go
     - Run go mod init

---
+++ errors.go
@@ -0,0 +1,12 @@
+package main 
+
+const (
+	errNoArg = "missing/invalid argument" 
+	errNoCmd = "missing/invalid command"
+        errBadArg = "unexpected argument supplied"
+	errBadTodo = "malformed .todo file"
+	errExists = "entry already exists"
+	errNoEntry = "no such entry"
+	errNoTodo = "no .todo file found"
+	errNoTask = "missing/invalid taks"
+)
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/halfwit/todo2
+
+go 1.16
+
+require github.com/goombaio/dag v0.0.0-20181006234417-a8874b1f72ff
